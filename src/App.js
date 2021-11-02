b382161bee151f466c5ecc88f0ce8ddacea47bc8
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-11-02 14:04:58 -0700

    added router to app.js and displaying information to route pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import NotesPage from "./pages/NotesPage";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route component={NotesPage} path="/" exact />
+        <Route component={NotePage} path="/note/:id" />
+      </div>
+    </Router>
   );
 }
 
